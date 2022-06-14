7d7b16a1a91bffbc01d2f7a2b6ca98328a8ed8f5
Beto Pereira <contato@betopereira.com>
2022-06-13 21:48:53 -0300

    Create tabs

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,19 @@ let btnCloseMenu = document.querySelector('.btn-close-menu').addEventListener('c
   let navigation = document.querySelector('.navigation');
   navigation.style.display = 'none';
 });
+
+// Switch portfolio tabs
+function openTab(e, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tab-link");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "grid";
+  e.currentTarget.className += " active";
+}
+document.getElementById("defaultOpen").click();
