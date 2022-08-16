c604cb79ea38b06259eaaf567be8c1e10ff7a064
Beto Pereira <contato@betopereira.com>
2022-08-16 19:13:17 -0300

    Deleted npm files and added the form script

--- js/server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express'),
-path = require('path'),
-nodeMailer = require('nodemailer');
-
-var app = express();
-var port = 3000;
-app.listen(port, function(req, res){
-  console.log('Server is running at port: ', port);
-});
